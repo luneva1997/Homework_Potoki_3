b4f66e892d7fbfc9e37576d0f46bde81ea3cbef4
Yulia Luneva <whatswrongnow@mail.ru>
2024-05-11 16:48:17 +0300

    Second chance to push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+    static AtomicInteger words3 = new AtomicInteger(0);
+    static AtomicInteger words4 = new AtomicInteger(0);
+    static AtomicInteger words5 = new AtomicInteger(0);
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        Random random = new Random();
+        String[] texts = new String[100_000];
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("abc", 3 + random.nextInt(3));
+        }
+
+        Future<AtomicInteger> task1 = executor.submit(new myCallable(3, words3, texts));
+        Future<AtomicInteger> task2 = executor.submit(new myCallable(4, words4, texts));
+        Future<AtomicInteger> task3 = executor.submit(new myCallable(5, words5, texts));
+        task1.get();
+        task2.get();
+        task3.get();
+
+        System.out.println("Красивых слов с длинной 3: " + words3.get() + " шт");
+        System.out.println("Красивых слов с длинной 4: " + words4.get() + " шт");
+        System.out.println("Красивых слов с длинной 5: " + words5.get() + " шт");
+
+        executor.shutdown();
+    }
+
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
 }
+
+
+
--- src/main/java/myCallable.java
+++ src/main/java/myCallable.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class myCallable {
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class myCallable implements Callable<AtomicInteger> {
+    int number;
+    AtomicInteger words;
+    String[] names;
+
+    public myCallable(int number, AtomicInteger words, String[] names) {
+        this.number = number;
+        this.words = words;
+        this.names = names;
+    }
+
+    @Override
+    public AtomicInteger call() throws Exception {
+
+        findPrettyNicknames(number, words, names);
+        return null;
+    }
+
+    public static void findPrettyNicknames (int number, AtomicInteger words, String[] names) {
+        for (String name : names) {
+            if (name.length() == number) {
+                if (rule1(name) | rule2(number, name) | rule3(name)) {
+                    words.addAndGet(1);
+                }
+            }
+        }
+    }
+
+    public static boolean rule1 (String name){
+        return name.contentEquals(new StringBuilder(name).reverse());
+    }
+
+    public static boolean rule2 (int number, String name) {
+        String first = "a".repeat(number);
+        String second = "b".repeat(number);
+        String third = "c".repeat(number);
+
+        return name.equals(first) | name.equals(second) | name.equals(third);
+    }
+
+    public static boolean rule3 (String name) {
+        char [] letters = name.toCharArray();
+        boolean answer = false;
+
+        for (int i = 0; i < letters.length-1; i++){
+            if (letters[i] < letters[i+1]){
+                answer = true;
+            } else {
+                answer = false;
+                break;
+            }
+        }
+        return answer;
+    }
 }
